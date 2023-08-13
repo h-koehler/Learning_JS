8dabf39f318abdbc0907c25a1bbe6271857bc4e1
koehlerh20 <140772335+koehlerh20@users.noreply.github.com>
2023-08-13 13:13:58 -0400

    Add files via upload

---
+++ Learning_JS/scripts/myScript_1.js
@@ -0,0 +1,7 @@
+function myFunction_5() {
+    document.getElementById("demo").innerHTML = "Paragraph Changed.";
+}
+
+function myFunction_6() {
+    document.getElementById("demo").innerHTML = "Yet another paragraph.";
+}
